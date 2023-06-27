8b33b40d9b9a672211801acb34c2a4a027aaf53b
Angel <Angelmarrufo79@gmail.com>
2023-06-27 16:05:29 -0700

    Agregar archivos de proyecto.

---
+++ Glifos/Grafos.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grafo
+{
+    internal class Grafos
+    {
+        private int numVertices;
+        private LinkedList<int>[] listaAdyacencia;
+
+        public Grafos(int vertices)
+        {
+            numVertices = vertices;
+            listaAdyacencia = new LinkedList<int>[vertices];
+            for (int i = 0; i < vertices; i++)
+            {
+                listaAdyacencia[i] = new LinkedList<int>();
+            }
+        }
+
+        public void AgregarArista(int origen, int destino)
+        {
+            listaAdyacencia[origen].AddLast(destino);
+            listaAdyacencia[destino].AddLast(origen);
+        }
+
+        public void MostrarGrafo()
+        {
+            for (int i = 0; i < numVertices; i++)
+            {
+                LinkedList<int> lista = listaAdyacencia[i];
+                Console.Write("Vertice " + i + " está conectado a: ");
+                foreach (var adyacente in lista)
+                {
+                    Console.Write(adyacente + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Glifos/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grafo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Grafos grafo = new Grafos(5);
+
+            grafo.AgregarArista(0, 1);
+            grafo.AgregarArista(0, 4);
+            grafo.AgregarArista(1, 2);
+            grafo.AgregarArista(1, 3);
+            grafo.AgregarArista(1, 4);
+            grafo.AgregarArista(2, 3);
+            grafo.AgregarArista(3, 4);
+
+            grafo.MostrarGrafo();
+
+            Console.ReadLine();
+        }
+    }
+}
